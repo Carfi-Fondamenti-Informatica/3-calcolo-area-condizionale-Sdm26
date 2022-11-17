0bcc7f0354f21f87d994258b0b1f8eeb30f81ceb
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-11-17 14:42:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+   float a,b,area;
+   int ozpione;
+   cin>>a>>b;
+   cin>>opzione;
+   if (
+   switch(opzione){
+      case 0:
+         area=a*b/2;
+         cout<<area<<endl;
+         break;
+      case 1:
+         area=a*a;
+         cout<<area<<endl;
+         break;
+      case 2:
+         area=a*b;
+         cout<<area<<endl;
+         break;
+      default:
+      cout<<"opzione non valida"<<endl;
+   }
    return 0;
 }
